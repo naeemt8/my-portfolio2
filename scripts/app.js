87deeab88074d0b2a5c8940212b49a54dc7dc260
naeem <naeemtaleghani.8@gmail.com>
2026-06-20 13:02:02 +0330

    adding carousel slider to "works" part

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,72 @@
+const project = document.querySelector('.project');
+const pervBtn = document.getElementById('pervBtn');
+const nextBtn = document.getElementById('nextBtn');
+const projectImg = document.getElementById('projectImg');
+const projectText = document.getElementById('projectText');
+
+const projectsList = [
+    {imgSrc: 'images/weather-project.png',text: '1', hrefLink: '#1'},
+    {imgSrc: 'images/dictionary-project.png',text: '2', hrefLink: '#2'},
+    {imgSrc: 'images/3.png',text: '3', hrefLink: '#3'},
+    {imgSrc: 'images/4.png',text: '4', hrefLink: '#4'}
+];
+
+let currentProject = 0;
+
+
+pervBtn.addEventListener('click', () =>{
+    console.log('perv');
+    if (currentProject == 0){
+        currentProject = projectsList.length-1;
+    }else{
+        currentProject -= 1; 
+    }
+    console.log('current = ', currentProject);
+    showPervproject();
+
+});
+nextBtn.addEventListener('click', () =>{
+    console.log('next');
+    if (currentProject == projectsList.length-1){
+        currentProject = 0;
+    }else{
+        currentProject += 1; 
+    }
+    console.log('current = ', currentProject);
+    showNextproject();
+});
+
+function showNextproject(){
+
+    project.classList.remove('animate-next', 'animate-perv');
+
+    // برای اینکه دوباره animation اجرا شود
+    void project.offsetWidth;
+
+
+    projectImg.src = projectsList[currentProject].imgSrc;
+    projectText.innerHTML = projectsList[currentProject].text;
+    project.href = projectsList[currentProject].hrefLink;
+
+
+    project.classList.add('animate-next');
+
+}
+function showPervproject(){
+
+    project.classList.remove('animate-next', 'animate-perv');
+
+    // برای اینکه دوباره animation اجرا شود
+    void project.offsetWidth;
+
+    projectImg.src = projectsList[currentProject].imgSrc;
+    projectText.innerHTML = projectsList[currentProject].text;
+    project.href = projectsList[currentProject].hrefLink;
+
+    project.classList.add('animate-perv');
+}
+
+
 import { CircularProgress } from "../circular-progress/circular-progress.js";
 
 window.customElements.define('cicular-progress', CircularProgress);
